f7b32115fc136c0bdb10de9e39cf9383d155b22f
AlanRSET <83605984+AlanRSET@users.noreply.github.com>
2021-05-27 21:00:07 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+import streamlit as st
+
+import keras
+from PIL import Image, ImageOps
+import numpy as np
+import h5py
+
+st.title("M mode image classification")
+st.header("")
+st.text("Upload a Image for image classification")
+
+from img_classification import teachable_machine_classification
+
+uploaded_file = st.file_uploader("Choose an Mmode image ...", type=['png','jpeg','jpg','dcm'])
+if uploaded_file is not None:
+        image = Image.open(uploaded_file)
+        st.image(image, caption='Uploaded Image.', use_column_width=True)
+        st.write("")
+        st.write("Classifying...")
+
+        hf = h5py.File('F:/streamlit-trial/VGG16Mmodegood.h5', 'r')
+        label = teachable_machine_classification(image, hf)
+        if label == 0:
+            st.write("The scan is of congestion")#cardeogenic pulmonary edema
+        elif label == 1:
+            st.write("The scan is of normal lung")
+        else:
+            st.write("The scan is of pleural effusion")
+        #st.write("The scan is of1 ", label)
